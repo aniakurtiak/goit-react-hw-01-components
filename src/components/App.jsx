2fdbf9efd84a9a4b6facea0c7ac158ee14b85383
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-09-01 00:34:29 +0300

    add props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,29 @@ import { FriendListItem } from "./friendListItem/fiendListItem";
 import { Profile } from "./profile/profile";
 import { Statistics } from "./statistics/statistics";
 import { TransactionHistory } from "./transactionHistory/transactionHistory";
-
+import user from "../json/user.json";
+import data from "../json/data.json";
+import friends from "../json/friends.json";
+import transactions from "../json/transactions.json";
+  
 export const App = () => {
   return (
     <div>
-      <Profile />
-      <Statistics />
-      <FriendList />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
       <FriendListItem />
-      <TransactionHistory/>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
