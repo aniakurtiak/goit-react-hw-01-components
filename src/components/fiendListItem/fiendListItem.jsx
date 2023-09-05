407ef2ab42527000055123efc5bb1234071e13d2
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-09-05 04:13:58 +0300

    isOnline

--- src/components/fiendListItem/fiendListItem.jsx
+++ src/components/fiendListItem/fiendListItem.jsx
@@ -3,7 +3,7 @@ import {Wrapper, Name, Status} from './fiendListItem.styled.js';
 export const FriendListItem = ({listItem: {isOnline, avatar, name}}) => {
     return (
         <Wrapper>
-            <Status $stat={isOnline}></Status>
+            <Status stat={isOnline}></Status>
             <img src={avatar} alt="User avatar" width="48" />
             <Name>{name}</Name>
         </Wrapper>
